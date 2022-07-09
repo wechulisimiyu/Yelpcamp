6ceac5485e0df830db4c6c568d6eaf4d3ce26e49
wechu07 <wechuli017@gmail.com>
2022-07-09 14:05:16 +0300

    session and flash

--- models/campground.js
+++ models/campground.js
@@ -3,27 +3,27 @@ const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-title: String,
-image: String,
-price: Number,
-description: String,
-location: String,
-reviews: [
-{
-type: Schema.Types.ObjectId,
-ref: 'Review'
-}
-]
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
-if (doc) {
-await Review.deleteMany({
-_id: {
-$in: doc.reviews
-}
-})
-}
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
