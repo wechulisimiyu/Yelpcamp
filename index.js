1fc5bb8bb7788b88a990fbb2f5ed1d17008761b0
wechu07 <wechuli017@gmail.com>
2022-07-12 19:04:38 +0300

    attempting deployment

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const reviewRoutes = require('./routes/reviews');
 const { func } = require('joi');
 const MongoDBStore = require('connect-mongo')(session);
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const dbUrlMongo = 'mongodb://localhost:27017/yelp-camp';
+// const dbUrlMongo = 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrlMongo, {
     useNewUrlParser: true,
